4f3d8d8f0969fc73aed427b5ed3527f98dbb43f5
Maurizio <maurizio.faedda1@gmail.com>
2020-10-13 16:04:03 +0200

    Calcolo km assegnando una destinazione

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-// chiedo all'utente quanti KM vuole percorrere
-var numeroChilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+// // chiedo all'utente quanti KM vuole percorrere
+// var numeroChilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+
+// Chiedo all'utente la destinazione tra Oristano(90km), Sassari(200km) e Olbia(260)
+var destinazione = prompt("La tua stazione di partenza è Cagliari. \nScrivi una destinazione tra Oristano, Sassari, Olbia:")
+
+// calcolo i km usando la scelta della destinazione
+if (destinazione == "Oristano") {
+    numeroChilometri = (90);
+} else if (destinazione === "Sassari") {
+    numeroChilometri = (200);
+} else if (destinazione === "Olbia") {
+    numeroChilometri = (260);
+}
 
 // Chiedo all'utente di inserire il suo anno di nascita
-var annoNascitaUtente = parseInt(prompt("Inserisci il tuo anno di nascita"));
+var annoNascitaUtente = parseInt(prompt("Inserisci il tuo anno di nascita:"));
 
 // Calcolo l'età dell'utente calcolando l'anno corrente meno la data di nascita
 var d = new Date();
 var etàUtente = d.getFullYear() - annoNascitaUtente;
-
 
 // prezzo biglietto
 var prezzoBiglietto = numeroChilometri * 0.21;
