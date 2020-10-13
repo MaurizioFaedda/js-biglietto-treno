3d66fa6d068946d1bb15a887fdd3f6e7af276e40
Maurizio <maurizio.faedda1@gmail.com>
2020-10-13 15:07:10 +0200

    Calcolo l'età chiedendo l'anno di nascita

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // chiedo all'utente quanti KM vuole percorrere
 var numeroChilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 
-// chiedo all'utente la sua ETA'
-var etàUtente = parseInt(prompt("Quanti anni hai?"));
+// Chiedo all'utente di inserire il suo anno di nascita
+var annoNascitaUtente = parseInt(prompt("Inserisci il tuo anno di nascita"));
+
+// Calcolo l'età dell'utente calcolando l'anno corrente meno la data di nascita
+var d = new Date();
+var etàUtente = d.getFullYear() - annoNascitaUtente;
+
 
 // prezzo biglietto
 var prezzoBiglietto = numeroChilometri * 0.21;
